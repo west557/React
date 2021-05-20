fab0a71e68e7807acb5eef00a37a2bb3d8730fa8
Scott West <west55721@gmail.com>
2021-05-21 07:04:10 +1000

    Initial Commit to Project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './index.css';
 import virtual from "./virtual.jpg"
 
 
+
 // https://api.github.com/users/west557
 // https://github.com/jasonhtpham/StoryAR-frontend
 
@@ -31,6 +32,25 @@ return (
     </div>
   );
 }
+
+/*
+
+return (
+  <h1> Create an Account now to get started! </h1>
+  <form>
+    <input type="First Name" placeholder = "First Name" name="first" />
+    <br> </br>
+    <input type="Surname" placeholder = "Surname" name="Surname" />
+    <br> </br>
+    <input type="text" placeholder="Email" name="email" />
+    <br> </br>
+    <input type="text" placeholder="Information" name="Information" />
+    </form>
+
+    );
+
+
+*/
 
 
 export default App;
